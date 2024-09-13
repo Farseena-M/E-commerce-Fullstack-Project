715713ed3789a58c0955a448fd6644fbbe1f9ed1
Farseena-M <farseenamoyan19@gmail.com>
2024-09-13 19:28:21 +0530

    token

--- backEnd/src/api/middleware/userAuth.js
+++ backEnd/src/api/middleware/userAuth.js
@@ -1,24 +1,30 @@
-const jwt=require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-module.exports=function verifyToken(req,res,next){
-    const authHeader=req.headers["authorization"];
+module.exports = function verifyToken(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    // console.log("Authorization Header:", authHeader);
+
     if (!authHeader) {
         return res.status(403).json({
             message: 'No token provided'
         });
     }
-    const token = authHeader;
-    console.log(token);
+
+    const token = authHeader.split(' ')[1];
+    // console.log("Extracted Token:", token);
+
     if (!token) {
         return res.status(403).json({
             message: 'Invalid token format'
         });
     }
-    jwt.verify(token,process.env.SECRET_STR,(err,decode)=>{
-        if(err) {
-            return res.status(401).json({message: "unauthorized"})
+
+    jwt.verify(token, process.env.SECRET_STR, (err, decode) => {
+        if (err) {
+            console.error("JWT Error:", err);
+            return res.status(401).json({ message: "Unauthorized" });
         }
-        req.email=decode.email
-        next()
-    })
-}
+        req.email = decode.email;
+        next();
+    });
+};
